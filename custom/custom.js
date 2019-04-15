be41b0df2ad7e5d0634a6b32383185e28c1ffaa6
Ali Borjian <web.ebox@gmail.com>
2019-04-15 14:18:53 +0430

    1

---
+++ custom/custom.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".docker-left").click(function () {
+        $(this).prev("div").toggle();
+        $(this).hide();
+    });
+    $(".docker-right").click(function () {
+        $(this).next("div").toggle();
+        $(this).hide();
+    });
+    $(".rotate").click(function () {
+        $(this).parent().parent().next().show();
+        $(this).parent().parent().next().next().show();
+    });
+
+
+});
